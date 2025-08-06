3362622fa9a5d010f013e2cb03ed90836bb67519
vl <goognya@gmail.com>
2025-08-06 20:57:31 +0100

    Ive changed style Home page with new photo typing

--- src/pages/Home/components/HomePage.jsx
+++ src/pages/Home/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Image, Stack, Text, useBreakpointValue } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 
@@ -11,45 +11,61 @@ const MotionButton = motion(Button)
 
 const HomePage = () => {
   const accessToken = useAuthStore((state) => state.accessToken)
+  const isMobile = useBreakpointValue({ base: true, md: false })
 
   notifyUser('accessToken', accessToken)
 
   return (
-    <Container position="relative" mb={{ base: 36, md: 0 }}>
-      <Stack direction={{ base: 'column', md: 'row' }} pb={16}>
-        <Flex p={{ base: 4, md: 8 }} pl={0} flex={1} align="center" justify="center" mt={16}>
-          <Stack spacing={4} justify={{ base: 'center', md: 'start' }} w={{ md: 'full' }}>
-            <Box>
-              <Heading as="h1" fontSize={{ base: '4xl', md: '5xl', lg: '6xl' }} textColor="#2D81A5" m={0} p={0}>
-                Testing the AUTH form
-              </Heading>
-              <Text fontSize={{ base: 'lg', lg: 'xl' }} color="teal.500">
-                Just fill out the request form, please
-              </Text>
-            </Box>
-            <MotionButton
-              colorScheme="teal"
-              w={56}
-              py={8}
-              as="a"
-              size="lg"
-              fontSize="3xl"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
+    <Container
+      position="relative"
+      mb={{ base: 36, md: 0 }}
+      minH="calc(100vh - 160px)"
+      display="flex"
+      alignItems="center"
+    >
+      <Stack direction={{ base: 'column-reverse', md: 'row' }} spacing={{ base: 8, md: 16 }} w="full" align="center">
+        <Flex flex={1} p={{ base: 4, md: 8 }} pl={0} direction="column" justify="center" mt={16}>
+          <Box mb={8}>
+            <Heading
+              as="h1"
+              fontSize={{ base: '3xl', sm: '4x1', md: '5xl', lg: '6xl' }}
+              color="teal.800"
+              lineHeight="1.2"
+              mb={4}
             >
-              <Link to="/signup">Make Now</Link>
-            </MotionButton>
-          </Stack>
+              Testing the AUTH form
+            </Heading>
+            <Text fontSize={{ base: 'md', lg: 'xl' }} color="teal.600">
+              Just fill out the request form, please
+            </Text>
+          </Box>
+          <MotionButton
+            as={Link}
+            to="/signup"
+            colorScheme="teal"
+            size={isMobile ? 'md' : 'lg'}
+            width={{ base: 'full', sm: 'auto' }}
+            maxWidth="380px"
+            height={{ base: '48px', md: '64px' }}
+            fontSize={{ base: 'xl', md: '2x1' }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            px={8}
+          >
+            Make Now
+          </MotionButton>
         </Flex>
         <Flex flex={1} justify="center" align="center" position="relative" overflow="hidden">
           <Image
             alt="Login image"
             objectFit="cover"
-            // src={
-            //   'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8' +
-            //   'fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-            // }
-            src="/images/photo-workspacetyping.jpg"
+            src="/images/typing_on_board_1919x1679.jpg"
+            fullbackSrc="/images/typing_on_board_600x525.jpg"
+            width="100%"
+            height="100%"
+            maxH="500px"
+            borderRadius="lg"
+            loading="eager" // Prioritize loading
           />
         </Flex>
       </Stack>

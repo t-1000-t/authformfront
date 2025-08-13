8775ad96c1fe09f78d215ccda75af48422aa8063
vl <goognya@gmail.com>
2025-08-13 21:08:09 +0100

    I have added the component works with Bot

--- src/pages/FooAgent/components/FooAgent.jsx
+++ src/pages/FooAgent/components/FooAgent.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react'
-import { Box } from '@chakra-ui/react'
+import React from 'react'
+import { Heading, Text, Link, Stack } from '@chakra-ui/react'
 import Container from '../../../layouts/Container'
-import useAuthStore from '../../../store/useAuthStore'
 
-const FooAgent = () => {
-  const { callFooAgent } = useAuthStore()
-
-  const id = '1234567890'
-
-  useEffect(() => {
-    callFooAgent(id).then()
-  }, [])
-  return (
-    <Container>
-      <Box>FOO</Box>
-    </Container>
-  )
-}
+const FooAgent = () => (
+  <Container>
+    <Stack gap={4}>
+      <Heading size="lg">Google Search Bot</Heading>
+      <Text>
+        Talk to
+        <Link href="https://t.me/goofoo_dev_bot" isExternal>
+          @goofoo_dev_bot
+        </Link>
+      </Text>
+      <Text fontSize="sm" opacity={0.8}>
+        The bot runs on our server. Your queries are handled privately on the backend.
+      </Text>
+    </Stack>
+  </Container>
+)
 
 export default FooAgent

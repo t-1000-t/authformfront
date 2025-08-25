f889d624e7a36dac1f42365f2e4bf9b670133f02
vl <goognya@gmail.com>
2025-08-25 14:38:43 +0100

    Ive fixed the small bag

--- src/pages/Cv/index.jsx
+++ src/pages/Cv/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react'
 import Cv from './components/Cv'
 import GeneratePdf from './Wrappers/GeneratePdf'
+import Container from '../../layouts/Container'
+import useAuthStore from '../../store/useAuthStore'
 
 const CvDefault = () => {
+  const { accessToken } = useAuthStore()
   return (
     <GeneratePdf>
-      <Cv />
+      {accessToken ? (
+        <Cv />
+      ) : (
+        <Container>
+          <Box>Form of CV are available only to logged-in users.</Box>
+        </Container>
+      )}
     </GeneratePdf>
   )
 }
